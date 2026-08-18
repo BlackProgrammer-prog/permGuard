7ed55cf366f9c6669a08335874c9252ec36acb0e
Parham Azizi <aziziparham2020@gmail.com>
2026-08-18 14:32:36 +0330

    Add server authorization exports
    Introduces the server package public API by exporting CASL ForbiddenError, the requireCan authorization guard, and permission snapshot helpers for safe ability serialization and enforcement.

---
+++ packages/server/src/index.ts
@@ -0,0 +1,6 @@
+export { ForbiddenError } from "@casl/ability";
+export { isForbiddenError, requireCan } from "./require-can.js";
+export {
+  createPermissionSnapshot,
+  type PermissionSnapshotAbility,
+} from "./snapshot.js";
